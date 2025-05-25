2753178635e6f5b7907ea5247431589ddc6163d5
Anshika Reja <154227997+Anshireja@users.noreply.github.com>
2025-05-26 01:34:07 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Anshika Reja</span> a passionate and driven Data Engineer in the making, currently pursuing a degree in Computer Science with a specialization in Data Science. With a strong foundation in programming languages like Python, C++, and SQL, I thrive on transforming raw data into meaningful insights. My technical toolkit includes tools like Pandas, NumPy, and Scikit-learn, along with experience in full-stack development using React.js, Node.js, and MongoDB.I’ve built projects ranging from AI-driven health assistants to scalable web applications for environmental sustainability. I also have hands-on experience with cloud platforms, machine learning models, and data manipulation, complemented by certifications from IBM and Google in Data Science and Analytics.When I’m not coding, I’m competing—having topped hackathons and coding competitions. Whether it’s building intelligent systems or crafting clean, responsive web interfaces, I love creating solutions that have real-world impact."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
